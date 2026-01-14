37ee88485f140535ca81572d17194aa5db83934c
Ong, Seth <seth.ong24@imperial.ac.uk>
2026-01-14 21:06:10 +0000

    [11.10 setup] create dummy endpoint in the app to do code changes and ensure that the deployed version has changed

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,10 @@ const App = () => {
     next = pokemonList.find(({ id }) => id === pokemonId + 1)
   }
 
+  app.get('/version', (req, res) => {
+    res.send('1')
+  })
+
   return (
     <Routes>
       <Route exact path="/" element={<PokemonList pokemonList={pokemonList} />} />
